cce290669c3a184c36f6a3cf1398b0be698b613d
Ashutosh Kumar <gaurav@suffescom.co>
2024-12-20 15:09:23 +0530

    initial commit

---
+++ internal/buildList.js
@@ -0,0 +1,31 @@
+const { version } = require("../package.json");
+
+const bsc = require("../tokens/bsc.json");
+
+
+module.exports = function buildList() {
+  const parsed = version.split(".");
+  return {
+    name: "Goblins Cash Menu",
+    timestamp: new Date().toISOString(),
+    version: {
+      major: +parsed[0],
+      minor: +parsed[1],
+      patch: +parsed[2],
+    },
+    tags: {},
+    logoURI:
+      "https://raw.githubusercontent.com/goblinscash/goblins-icons/main/blockchains/smartchain/assets/0x701ACA29AE0F5d24555f1E8A6Cf007541291d110/logo.png",
+    keywords: ["goblinscash", "default"],
+    tokens: [     
+      ...bsc,
+    ]
+      // sort them by symbol for easy readability
+      .sort((t1, t2) => {
+        if (t1.chainId === t2.chainId) {
+          return t1.symbol.toLowerCase() < t2.symbol.toLowerCase() ? -1 : 1;
+        }
+        return t1.chainId < t2.chainId ? -1 : 1;
+      }),
+  };
+};
---
+++ internal/chain.js
@@ -0,0 +1,5 @@
+export const ChainId = {
+    BSC: 56,
+    BASE: 8453,
+    SMARTBCH: 10000
+}
